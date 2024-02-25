d49533146c66fc553c84247a7ce88c3f3758c86f
Sadra Saharkhiz <sarkh200@gmail.com>
2024-02-25 12:14:34 -0500

    Added comments

--- ESP32-s3/MQTT RGB/src/main.cpp
+++ ESP32-s3/MQTT RGB/src/main.cpp
@@ -2,22 +2,33 @@
 #include <PubSubClient.h>
 #include <WiFi.h>
 
+/* Create PrivateInfo.h in in ./include with values for:
+    char ssid[]
+    char password[]
+    int mqtt_broker
+    int mqtt_port
+    char client_id[]
+    char subTopic[]*/
 #include "PrivateInfo.h"
 
+// Arbitrary length for maximum length of message that can be read from topic (Can be changed to any value)
 #define MaxMessageLength 100
 
 // PubSubClient instance
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+// Function prototypes here:
 void callback(char *topic, byte *payload, unsigned int length);
 
 void setup()
 {
+  // Begin Serial
   Serial.begin(115200);
 
   delay(20);
 
+  // Connect to WiFi
   Serial.print("Connecting to WiFi");
   WiFi.begin(ssid, password);
   while (!WiFi.isConnected())
@@ -29,6 +40,7 @@ void setup()
   Serial.printf("Connected to %s as ", WiFi.SSID());
   Serial.println(WiFi.localIP());
 
+  // Connect to MQTT
   Serial.print("Connecting to MQTT broker: ");
   Serial.println(mqtt_broker);
   client.setServer(mqtt_broker, mqtt_port);
@@ -46,8 +58,10 @@ void setup()
     }
   }
 
+  // Set function to run on message received
   client.setCallback(callback);
 
+  // Subscribe to topic
   Serial.printf("Subscribing to topic: %s\n", subTopic);
   client.subscribe(subTopic);
   Serial.printf("Subscribed to: %s\n", subTopic);
@@ -55,24 +69,32 @@ void setup()
 
 void loop()
 {
+  // Must be run to keep connection alive
   client.loop();
 }
 
+// Gets run everytime a new message is published
 void callback(char *topic, byte *payload, unsigned int length)
 {
-  char message[12] = {'0'};
+  // char array to store payload
+  char message[MaxMessageLength] = {'0'};
+  // ints to store rgb values
   int r = 0, g = 0, b = 0;
 
+  // Trims message down to be under MaxMessageLength
   if (length > MaxMessageLength)
   {
     length = MaxMessageLength - 1;
   }
+  // Stored payload as char[]
   for (int i = 0; i < length; i++)
   {
     message[i] = (char)payload[i];
   }
 
+  // Gets rgb values from message
   sscanf(message, "%d %d %d", &r, &g, &b);
 
+  // Sets onboard LED to values defined in message
   neopixelWrite(RGB_BUILTIN, r, g, b);
 }
