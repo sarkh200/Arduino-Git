38f75fa4c807b8cf5553c181da91035e99dc4996
Sadra Saharkhiz <sarkh200@gmail.com>
2024-02-26 01:33:33 -0500

    setup servos for controlling

---
+++ ESP32-s3/Senior Project/src/main.cpp
@@ -0,0 +1,120 @@
+#include <Arduino.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
+#include <ESP32Servo.h>
+
+/* Create PrivateInfo.h in in ./include with values for:
+    char ssid[]
+    char password[]
+    int mqtt_broker
+    int mqtt_port
+    char client_id[]
+    char subTopic[]*/
+#include "PrivateInfo.h"
+
+#define motorPin1 4
+#define motorPin2 5
+#define motorPin3 6
+
+// Arbitrary length for maximum length of message that can be read from topic (Can be changed to any value)
+#define MaxMessageLength 100
+
+// PubSubClient instance
+WiFiClient espClient;
+PubSubClient client(espClient);
+// Servo
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+// Function prototypes here:
+void callback(char *topic, byte *payload, unsigned int length);
+
+void setup()
+{
+  // Begin Serial
+  Serial.begin(115200);
+
+  // setup servos
+  servo1.setPeriodHertz(50);
+  servo1.attach(motorPin1);
+  servo2.setPeriodHertz(50);
+  servo2.attach(motorPin2);
+  servo3.setPeriodHertz(50);
+  servo3.attach(motorPin3);
+
+  delay(20);
+
+  // Connect to WiFi
+  Serial.print("Connecting to WiFi");
+  WiFi.begin(ssid, password);
+  while (!WiFi.isConnected())
+  {
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println();
+  Serial.printf("Connected to %s as ", WiFi.SSID());
+  Serial.println(WiFi.localIP());
+
+  // Connect to MQTT
+  Serial.print("Connecting to MQTT broker: ");
+  Serial.println(mqtt_broker);
+  client.setServer(mqtt_broker, mqtt_port);
+
+  while (!client.connected())
+  {
+    if (client.connect(client_id))
+    {
+      Serial.println("Connected to MQTT broker!");
+    }
+    else
+    {
+      Serial.printf("Connection failed to MQTT broker; client state = %d\n Trying again in 10 seconds...", client.state());
+      delay(10000);
+    }
+  }
+
+  // Set function to run on message received
+  client.setCallback(callback);
+
+  // Subscribe to topic
+  Serial.printf("Subscribing to topic: %s\n", subTopic);
+  client.subscribe(subTopic);
+  Serial.printf("Subscribed to: %s\n", subTopic);
+}
+
+void loop()
+{
+  // Must be run to keep connection alive
+  client.loop();
+}
+
+// Gets run everytime a new message is published
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  // char array to store payload
+  char message[MaxMessageLength] = {'0'};
+  // ints to store rgb values
+  int x = 0, y = 0, z = 0;
+
+  // Trims message down to be under MaxMessageLength
+  if (length > MaxMessageLength)
+  {
+    length = MaxMessageLength - 1;
+  }
+  // Stored payload as char[]
+  for (int i = 0; i < length; i++)
+  {
+    message[i] = (char)payload[i];
+  }
+
+  // Gets rgb values from message
+  sscanf(message, "%d %d %d", &x, &y, &z);
+
+  // Sets onboard LED to values defined in message
+  Serial.printf("Setting arm to: (%d, %d, %d)\n", x, y, z);
+  servo1.write(x);
+  servo2.write(y);
+  servo3.write(z);
+}
